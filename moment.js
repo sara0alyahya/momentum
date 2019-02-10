952eb99db4b6794960628424aad0ac3d7a3e0646
sara0alyahya <sara.a.alyahya@outlook.com>
2019-02-10 20:14:44 +0300

    final commoit

--- moment.js
+++ moment.js
@@ -1,3 +1,21 @@
 console.log(moment().format('h:mm A'));
 console.log(moment().format(' A'));
 $('body').append('<h2>' + moment().format('h:mm A') + '</h2>')
+
+const hours = new Date().getHours();
+var message;
+const morning = ('Good Morning');
+const afternoon = ('Good Afternoon');
+const evening = ('Good Evening');
+
+if (hours >= 0 && hours < 12) {
+    message = morning + ' , SARA';
+
+} else if (hours >= 12 && hours < 17) {
+    message = afternoon + ' , SARA';
+
+} else if (hours >= 17 && hours < 24) {
+    message = evening + ' , SARA';
+}
+
+$('.greeting').append(message);
